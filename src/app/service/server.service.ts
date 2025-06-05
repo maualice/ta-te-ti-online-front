93baa0ff776df5c37efad95107612475d1d06fd2
Mauro Alice <alicemauroe@gmail.com>
2025-06-05 20:53:21 -0300

    agregando servicio para usar websockets

---
+++ src/app/service/server.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { io } from 'socket.io-client';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ServerService {
+
+  server = io("localhost:3000", { autoConnect: false }) //se crea instancia de un cliente socket.io,y no se va a conectar automaticamente al crear la instancia
+
+  constructor() {
+    this.server.on("connect",()=>{//listener para el evento connect del socket,cuando se dispare evento connect ejecuta esta funcion
+      console.log("Conectado al back");   
+    });
+    this.server.connect();//se inicia la conexión manualmente
+   }
+
+}
