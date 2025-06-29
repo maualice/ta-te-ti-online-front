09b1268c44a96dc7c54027320d6ff16a183140a3
Mauro Alice <alicemauroe@gmail.com>
2025-06-29 17:54:53 -0300

    estados del juego: comenzando a jugar poniendo marcas en el tablero,sincronizacion de clientes y compartiendo datos en tiempo real,destructuracion de salas, turno de jugadores

--- src/app/pages/jugar/jugar.component.ts
+++ src/app/pages/jugar/jugar.component.ts
@@ -2,28 +2,34 @@ import { Component, inject, input, OnInit } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { ServerService } from '../../services/server.service';
 import { UsuarioService } from '../../services/usuario.service';
+import { DetallePartidaComponent } from '../../components/detalle-partida/detalle-partida.component';
+import { TableroComponent } from '../../components/tablero/tablero.component';
+import { SalaService } from '../../services/sala.service';
 
 @Component({
   selector: 'app-jugar',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, DetallePartidaComponent, TableroComponent],
   templateUrl: './jugar.component.html',
   styleUrl: './jugar.component.scss'
 })
 export class JugarComponent implements OnInit {
 
-  serverService = inject(ServerService)
-  usuarioService = inject(UsuarioService)
-  esPrivada = input()
-  id = input<string>()
+  serverService = inject(ServerService);
+  usuarioService = inject(UsuarioService);
+  salaService = inject(SalaService)
+  esPrivada = input();
+  id = input<string>();
+
 
   ngOnInit(): void {
     if (!this.esPrivada() && !this.id()) {
-      this.serverService.crearSala()
+      this.salaService.crearSala();
     } else if (this.id()) {
-      this.serverService.unirseASala(parseInt(this.id()!)) //si llego hasta aca entonces es porque existe
+      console.log("Intentando unirse a la sala", this.id())
+      this.salaService.unirseASala(parseInt(this.id()!));
     } else {
-      this.serverService.crearSala(true)
+      this.salaService.crearSala(true);
     }
   }
 }
--- src/app/services/server.service.ts
+++ src/app/services/server.service.ts
@@ -1,8 +1,8 @@
 import { inject, Injectable } from '@angular/core';
 import { io } from 'socket.io-client';
 import { UsuarioService } from './usuario.service';
-import { CrearSalaArgs } from '../interfaces/crearSala';
-import { UnirseASalaArgs } from '../interfaces/unirseASala';
+import { SalaBackend } from '../interfaces/sala';
+import { Subject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -12,32 +12,15 @@ export class ServerService {
   server = io("localhost:3000", { autoConnect: false }) //se crea instancia de un cliente socket.io,y no se va a conectar automaticamente al crear la instancia
   usuarioService = inject(UsuarioService)
 
+  actualizacionDeSala$ = new Subject<SalaBackend>()
+
   constructor() {
     this.server.on("connect", () => {//listener para el evento connect del socket,cuando se dispare evento connect ejecuta esta funcion
       console.log("Conectado al back");
     });
-    this.server.on("sala",(args)=>console.log(args))
+    this.server.on("sala", (args) => {
+      this.actualizacionDeSala$.next(args)
+    })
     this.server.connect();//se inicia la conexión manualmente
   }
-
-  crearSala(esPrivada: boolean = false) {
-    const args: CrearSalaArgs = {
-      publica: !esPrivada,
-      nombreJugador: this.usuarioService.nombre()
-    }
-    this.server.emitWithAck('crearSala', args).then(res => {
-      console.log("Crear Sala", res);
-    })
-  }
-
-  unirseASala(id: number) {
-    const args: UnirseASalaArgs = {
-      id,
-      nombreJugador: this.usuarioService.nombre()
-    }
-    this.server.emitWithAck('unirseASala', args).then(res => {
-      console.log("Resutlado de union a sala", res);
-    })
-  }
-
 }
